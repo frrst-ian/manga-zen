ea44acb98b07d875617cc3eb76ea2936fcdf46a0
frrst-ian <frrstxd@proton.me>
2025-02-28 22:05:51 +0800

    chore: rename homeRouter to indexRouter

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
-const homeRouter = require("./routes/homeRouter");
-require('dotenv').config();
+const indexRouter = require("./routes/indexRouter");
+require("dotenv").config();
 
 app.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: true }));
 
-app.use("/", homeRouter);
+app.use(express.urlencoded({ extended: true }));
+app.use("/", indexRouter);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Express app is listening on PORT ${PORT}.`));
+
+app.listen(PORT, () => console.log(`Express app listening on port ${PORT}`));
