995809125227f4dda00e5e399b59255e620502e7
frrst-ian <frrstxd@proton.me>
2025-02-28 12:55:57 +0800

    fix: redefine the router variable app.js and use

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
-// const router = require("./routes/");
+const homeRouter = require("./routes/homeRouter");
 require('dotenv').config();
 
 app.set("view-engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
-// app.use("/", router);
+app.use("/", homeRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Express app is listening on PORT ${PORT}.`));
