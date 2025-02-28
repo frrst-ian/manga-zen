7b30f02993d770dab1c3a006fe80b4de81cd7fbb
frrst-ian <frrstxd@proton.me>
2025-02-28 11:57:52 +0800

    feat: add home controller to control the logic of the home page, this involves fetching the mangas from the db

---
+++ controllers/homeController.js
@@ -0,0 +1,21 @@
+const db = require("../db/queries")
+
+async function getAllMangaHandler(req, res) {
+    const manga_books = await db.getAllManga();
+    res.render("index", { manga_books });
+
+}
+
+async function getMangaByIdHandler(req,res) {
+    try {
+        const manga = await getMangaByIdHandler(req.params.id);
+        if(manga) {
+            res.render("manga" , {manga})
+        } else {
+            res.status(404).send("Manga not found");
+        }
+    } catch (error) {
+        console.error("Error fetching manga: " , error);
+        res.status(505).send("Internal Server Error");
+    }
+}
--- routes/homeRouter.js
+++ routes/homeRouter.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const homeRouter = Router();
 const homeController = ("../controllers/homeController");
 
-homeRouter.get("/", getAllManga);
-homeRouter.get("/manga/:id", getMangaById);
+homeRouter.get("/", getAllMangaHandler);
+homeRouter.get("/manga/:id", getMangaByIdHandler);
 
 module.exports = homeRouter;
