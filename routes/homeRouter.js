6c29c36240405fca2a5129a79bff44bb016e5245
frrst-ian <frrstxd@proton.me>
2025-02-28 21:55:35 +0800

    fix: refactor codebase to imrpove overall structure

--- routes/homeRouter.js
+++ routes/homeRouter.js
@@ -1,8 +1,16 @@
 const { Router } = require("express");
 const homeRouter = Router();
 const homeController = require("../controllers/homeController");
+const mangaController = require("../controllers/mangaController");
+const genreController = require("../controllers/genreController");
+const addMangaController = require("../controllers/addMangaController");
+const addGenreController = require("../controllers/addGenreController")
 
-homeRouter.get("/", homeController.getAllMangaHandler);
-homeRouter.get("/manga/:id", homeController.getMangaByIdHandler);
-
-module.exports = homeRouter;
+homeRouter.get("/", homeController.getIndex);
+homeRouter.get("/genres", genreController.getAllGenres);
+homeRouter.get("/genre/:id", genreController.getMangaByGenre);
+homeRouter.get("/genre/:id/manga-info", genreController.getMangaInfo)
+homeRouter.get("/manga", mangaController.getAllManga);
+homeRouter.get("/manga/:id", mangaController.getMangaById);
+homeRouter.get("/add-manga", addMangaController.addManga);
+homeRouter.get("/add-genre", addGenreController.addGenre);
