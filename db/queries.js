debc607b4ec99f83baab5ffd6017d0677dfe92a3
frrst-ian <frrstxd@proton.me>
2025-03-01 16:02:21 +0800

    fix: set an id to the manga query to show accurate info

--- db/queries.js
+++ db/queries.js
@@ -6,7 +6,7 @@ async function getAllManga() {
     return rows;
 }
 
-async function getMangaById() {
+async function getMangaById(id) {
     const SQLQuery = (`SELECT mb.id, mb.name, mb.published_year,
 string_agg(DISTINCT a.author_name,', ') as authors,
 string_agg(DISTINCT g.genre_name,', ') as genres
@@ -15,8 +15,9 @@ LEFT JOIN manga_authors ma ON mb.id = ma.id
 LEFT JOIN manga_genres mg ON mb.id = mg.id
 LEFT JOIN authors a ON ma.author_id = a.author_id
 LEFT JOIN genres g ON mg.genre_id = g.genre_id
+WHERE mb.id = $1
 GROUP BY mb.id, mb.name, mb.published_year;`);
-    const { rows } = await pool.query(SQLQuery);
+    const { rows } = await pool.query(SQLQuery , [id]);
     return rows[0];
 }
 
