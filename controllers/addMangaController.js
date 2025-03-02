760bbe2154703c07ba1702838c9ca2448aad6603
frrst-ian <frrstxd@proton.me>
2025-03-02 14:28:28 +0800

    feat : update mangaController to allow image_path to be passed to the db

--- controllers/addMangaController.js
+++ controllers/addMangaController.js
@@ -1,8 +1,13 @@
 const db = require("../db/queries");
+const fs = require('fs');
+const path = require('node:path');
 
 async function addMangaHandler(req, res) {
     try {
-        const { manga_name, published_year, image_path, genre_names, author_name } = req.body;
+        const { manga_name, published_year, genre_names, author_name } = req.body;
+
+        const image_path = `/manga/images/${req.file.filename}`;
+
         await db.addManga(manga_name, published_year, image_path, genre_names, author_name);
         res.redirect("/");
     } catch (err) {
