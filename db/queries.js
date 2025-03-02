29a81aa322adfd3f53e1bbfe4147b4a0b784509c
frrst-ian <frrstxd@proton.me>
2025-03-02 10:04:37 +0800

    feat: add manga cover photo in the db schema and fetch in the controllers

--- db/queries.js
+++ db/queries.js
@@ -7,7 +7,7 @@ async function getAllManga() {
 }
 
 async function getMangaById(id) {
-    const SQLQuery = (`SELECT mb.id, mb.name, mb.published_year,
+    const SQLQuery = (`SELECT mb.id, mb.name, mb.published_year,mb.image_path,
 string_agg(DISTINCT a.author_name,', ') as authors,
 string_agg(DISTINCT g.genre_name,', ') as genres
 FROM manga_books mb
@@ -16,8 +16,9 @@ LEFT JOIN manga_genres mg ON mb.id = mg.id
 LEFT JOIN authors a ON ma.author_id = a.author_id
 LEFT JOIN genres g ON mg.genre_id = g.genre_id
 WHERE mb.id = $1
-GROUP BY mb.id, mb.name, mb.published_year;`);
+GROUP BY mb.id, mb.name, mb.published_year, mb.image_path;`);
     const { rows } = await pool.query(SQLQuery, [id]);
+    console.log(rows[0])
     return rows[0];
 }
 
@@ -29,7 +30,7 @@ async function getAllGenre() {
 
 async function getGenreById(genreId) {
     const SQLQuery = (`
-SELECT mb.id, mb.name, mb.published_year,
+SELECT mb.id, mb.name, mb.published_year,mb.image_path,
 string_agg(DISTINCT a.author_name,', ') as authors
 FROM manga_books mb
 LEFT JOIN manga_authors ma ON mb.id = ma.id
@@ -37,7 +38,7 @@ LEFT JOIN manga_genres mg ON mb.id = mg.id
 LEFT JOIN authors a ON ma.author_id = a.author_id
 LEFT JOIN genres g ON mg.genre_id = g.genre_id
 WHERE g.genre_id = $1
-GROUP BY mb.id, mb.name, mb.published_year;
+GROUP BY mb.id, mb.name, mb.published_year, mb.image_path;
 `);
     const { rows } = await pool.query(SQLQuery , [genreId]);
     return rows;
