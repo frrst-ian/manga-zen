e2b3ba5c0286824ddf4373b6c4f3240b227eb202
frrst-ian <frrstxd@proton.me>
2025-03-02 19:50:24 +0800

    chore: add code comments on app.js for code readability

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
+// Load .env 
 require("dotenv").config();
+// Get vital modules
 const express = require("express");
-const app = express();
+const multer = require('multer');
 const path = require("node:path");
-const assetsPath = path.join(__dirname, "public");
+// Get express method
+const app = express();
+// Get router modules
 const indexRouter = require("./routes/indexRouter");
 const mangaRouter = require("./routes/mangaRouter");
 const genresRouter = require("./routes/genresRouter");
 const addMangaRouter = require("./routes/addMangaRouter");
-const multer = require('multer')
-
+// Get the assets path ,esp. public
+const assetsPath = path.join(__dirname, "public");
+// Use ejs as view engine
 app.set("views", path.join(__dirname, "views"));
 app.set('view engine', 'ejs');
+// Use express.static to serve static files
 app.use(express.static(assetsPath))
+// Use express.urlencoded to convert req.body to JS object
 app.use(express.urlencoded({ extended: true }));
-
+// Define middleware functions
 app.use("/", indexRouter);
 app.use("/manga", mangaRouter);
 app.use("/genres", genresRouter);
 app.use("/add-manga", addMangaRouter);
 
-// Add this error handler after your routes
+
 app.use((err, req, res, next) => {
   if (err instanceof multer.MulterError) {
     return res.status(400).send(`File upload error: ${err.message}`);
