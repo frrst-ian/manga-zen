f844bafb7acb04ddbf6b4918e0e7e76723c97ef1
frrst-ian <frrstxd@proton.me>
2025-04-20 18:19:02 +0800

    fix: add parameters to handle limit on render

--- db/pool.js
+++ db/pool.js
@@ -2,7 +2,9 @@ const { Pool } = require("pg")
 require('dotenv').config();
 module.exports = new Pool({
     connectionString: process.env.DB_URL,
-    ssl: {
-        rejectUnauthorized: false,
-    }
+    ssl: { rejectUnauthorized: true },
+    max: 3, 
+    idleTimeoutMillis: 30000, 
+    connectionTimeoutMillis: 5000,
+    allowExitOnIdle: true  
 })
